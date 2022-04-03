14d5b747771bfc4e15732954d3e04610adb62a95
jessica tam <jessica.tam128@gmail.com>
2022-04-02 23:49:28 -0400

    add createObservation submit handler for the POST function

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,6 +4,27 @@
  */
 const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
 
+const observations = [];
+
+
+function nextId() {
+  const uint32 = window.crypto.getRandomValues(new Uint32Array(1))[0];
+  return uint32.toString(16);
+}
+
+export async function createObservation(observation, signal) {
+  const now = new Date().toISOString();
+  const newObservation = {
+    ...observation,
+    observation_id: nextId(),
+    created_at: now,
+    updated_at: now,
+  };
+  observations.push(newObservation);
+  return newObservation;
+}
+
+
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
@@ -24,21 +45,26 @@ headers.append("Content-Type", "application/json");
  *  If the response is not in the 200 - 399 range the promise is rejected.
  */
 async function fetchJson(url, options) {
-  try {
+  try
+  {
     const response = await fetch(url, options);
 
-    if (response.status === 204) {
+    if (response.status === 204)
+    {
       return null;
     }
 
     const payload = await response.json();
 
-    if (payload.error) {
+    if (payload.error)
+    {
       return Promise.reject({ message: payload.error });
     }
     return payload.data;
-  } catch (error) {
-    if (error.name !== "AbortError") {
+  } catch (error)
+  {
+    if (error.name !== "AbortError")
+    {
       console.error(error.stack);
       return Promise.reject({ message: error.message });
     }
